abe57573629604f9229a93d18f1e75a631561a9f
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-08-01 21:34:30 -0300

    summary

--- src/components/Opcoes-header/index.js
+++ src/components/Opcoes-header/index.js
@@ -1,14 +1,32 @@
 import './style.css'
+import styled from 'styled-components'
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA BIBLIOTECA']
 
+const Opcoes = styled.ul`
+    display: flex;
+ `
+
+ const Opcao = styled.li`
+      min-width: 8rem;
+      font-size: 1rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      height: 100%;
+      padding: 0 0.5rem;
+      cursor: pointer;
+      list-style: none;
+ `
+
 function OpcoesHeader (){
  return(
-      <ul className='opcoes'>
+      <Opcoes>
           { textoOpcoes.map( (texto) => (
-            <li className='opcao'><p>{texto}</p></li>
+            <Opcao><p>{texto}</p></Opcao>
           ))}
-        </ul>
+        </Opcoes>
  )       
 }
 
