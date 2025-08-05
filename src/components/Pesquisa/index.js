33b3e3ecf1194193cb03400688473e95f0e234f1
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-08-05 17:05:13 -0300

    Add search results display to Pesquisa component
    Introduces a new Resultado styled component and updates Pesquisa to render a list of searched books with their names and cover images. The search results are now displayed dynamically based on user input.

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -24,11 +24,26 @@ const Subtitulo = styled.h3`
         font-weight: 500;
         margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
 function Pesquisa() {
-    const [livrosPesquisados, setLivrosPesquisados] = useState()
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
 
-    console.log(livrosPesquisados)
         return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
@@ -41,6 +56,14 @@ function Pesquisa() {
                         setLivrosPesquisados(resultadoPesquisa)
                 }}   
              />
+        { livrosPesquisados.map( livro => (
+                <Resultado>
+                        <p>{livro.nome}</p>
+                        <img src = {livro.src}
+                        alt="livro capa"
+                        />       
+                </Resultado>
+        ) ) }
         </PesquisaContainer>
 
       
