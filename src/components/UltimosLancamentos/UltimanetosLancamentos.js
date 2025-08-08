2bd9ba9dd1ec65bcc2bc1418b3afdab73b15d5bb
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-08-07 23:30:58 -0300

    Refactor Titulo component and update usage
    Extracted the Titulo styled component into its own file for reusability and updated UltimosLancamentos to import and use the new Titulo component with a customizable color prop.

--- src/components/UltimosLancamentos/UltimanetosLancamentos.js
+++ src/components/UltimosLancamentos/UltimanetosLancamentos.js
@@ -1,5 +1,6 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
+import { Titulo } from '../../components/Titulo/index'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -8,15 +9,6 @@ const UltimosLancamentosContainer = styled.section`
     flex-direction: column;
 `
 
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
-`
 
 const NovosLivrosContainer = styled.div`
     margin-top: 30px;
@@ -29,7 +21,7 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos(){
     return(
         <UltimosLancamentosContainer>
-            <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+            <Titulo cor = '#ff9900ff'>ÚLTIMOS LANÇAMENTOS</Titulo>
             <NovosLivrosContainer>
                  {livros.map( livro => (
                     <img
